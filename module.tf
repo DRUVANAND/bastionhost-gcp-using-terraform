04904bd6225810178659fd305fbd246a47fecf7f
DHRUVOPS360 <128677270+DHRUVOPS360@users.noreply.github.com>
2023-05-18 18:42:22 +0530

    Update module.tf

--- module.tf
+++ module.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.58.0"
+    }
+  }
+backend "gcs" {
+    bucket  = "dhruv-bastion-host-state-files"
+    prefix  = "terraform-bastion-host"
+  }
+}
+
+provider "google" {
+  project = "engineer-cloud-nprod"
+  region        = var.region
+  zone         = var.zone
+}
+
 module "bastion" {
   source = "./src"
 }
